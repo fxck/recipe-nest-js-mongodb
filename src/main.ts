f16924b11b409c6ce62aed5808b759d986938dbe
Aleš <foxxcz@gmail.com>
2021-11-08 18:52:11 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    next();
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   await app.listen(3000);
 }
+
 bootstrap();
