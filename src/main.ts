5fd9a64821db68d16f72647e5762c05c8360a498
Petr Klika <pe.klika@gmail.com>
2021-11-08 14:12:35 +0100

    cors change

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-    {
-      cors: {
-        origin: false,
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        preflightContinue: false
-      }
-    }
-  );
+  const app = await NestFactory.create(AppModule);
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*'
+});
+
   await app.listen(3000);
 }
 bootstrap();
